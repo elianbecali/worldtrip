c5321e2c8c4fdc1d3d4078ba5c589062deeaa9b5
Elian Becali <elianbecaliaguiar@gmail.com>
2021-07-12 18:55:44 -0400

    fix(header): add link to back home when not home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,43 @@
 import Link from 'next/link';
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Grid, Image, Icon } from '@chakra-ui/react';
+import { RiArrowLeftSLine } from 'react-icons/ri';
+import { useRouter } from 'next/router';
 
 export function Header() {
+  const { asPath } = useRouter();
+  const notHomePage = asPath !== '/';
+
   return (
     <Flex as="header" justifyContent="center" padding="7">
-      <Link href="/">
-        <a>
-          <Image src="/images/Logo.svg" alt="Logo Worldtrip" />
-        </a>
-      </Link>
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+        alignSelf="start"
+      >
+        {notHomePage && (
+          <Link href="/">
+            <a>
+              <Icon
+                as={RiArrowLeftSLine}
+                fontSize={[20, 40]}
+                color="dark.heading"
+                justifySelf="start"
+              />
+            </a>
+          </Link>
+        )}
+        <Image
+          src="/images/Logo.svg"
+          alt="Logo Worldtrip"
+          gridColumn="2"
+          justifySelf="center"
+        />
+      </Grid>
     </Flex>
   );
 }
