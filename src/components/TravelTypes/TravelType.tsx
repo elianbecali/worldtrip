223d698bd05084ba3a81652138df52fde239d48f
Elian Becali <elianbecaliaguiar@gmail.com>
2021-07-13 07:12:31 -0400

    fix: adjust to responsive travel type section

--- src/components/TravelTypes/TravelType.tsx
+++ src/components/TravelTypes/TravelType.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, VStack } from '@chakra-ui/react';
+import { Image, Text, Flex, useBreakpointValue } from '@chakra-ui/react';
 
 interface TravelTypeProps {
   children: string;
@@ -6,12 +6,33 @@ interface TravelTypeProps {
 }
 
 export function TravelType({ children, icon }: TravelTypeProps) {
+  const isMobile = useBreakpointValue({
+    base: false,
+    sm: true
+  });
+
   return (
-    <VStack>
-      <Image src={`/images/icons/${icon}.svg`} size="85px" />
-      <Text color="dark.heading" fontWeight="semibold" mt="2">
+    <Flex
+      flexDirection={['row', 'column']}
+      alignItems="center"
+      justifyContent="center"
+    >
+      {isMobile ? (
+        <Image src={`/images/icons/${icon}.svg`} size="85px" />
+      ) : (
+        <Text color="highlight" fontSize="4xl" mr="2">
+          .
+        </Text>
+      )}
+
+      <Text
+        color="dark.heading"
+        fontWeight="medium"
+        fontSize={['1.12rem', '1.5rem']}
+        mt={['0', '2']}
+      >
         {children}
       </Text>
-    </VStack>
+    </Flex>
   );
 }
